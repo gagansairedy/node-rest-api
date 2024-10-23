292ff54b8e79a0364c8ba6a4d5b96b3e73ca0f22
Gagan Reddy <gaganreddy@Gagans-MacBook-Pro.local>
2024-10-23 14:20:43 -0700

    initial commit

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ app.get('/add', (req, res) => {
     const num1 = parseFloat(req.query.num1);
     const num2 = parseFloat(req.query.num2);
     const sum = num1 + num2;
-    res.send(`Sum is ${sum}`);
+    res.json({ result: `Sum is ${sum}` });  // Return JSON response
 });
 
 // POST method
 app.post('/add', (req, res) => {
     const { num1, num2 } = req.body;
     const sum = num1 + num2;
-    res.send(`Sum is ${sum}`);
+    res.json({ result: `Sum is ${sum}` });  // Return JSON response
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
